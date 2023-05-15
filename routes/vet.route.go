81f8946f9b1d4ebbcd02b487e5e4aead0b14c11e
Julian Ardila <julian07099@gmail.com>
2023-05-14 23:36:54 -0500

    Added endpoint for professional of the vet and partner

--- routes/vet.route.go
+++ routes/vet.route.go
@@ -8,5 +8,6 @@ import (
 func setRouterVet(apiRouter *mux.Router) {
 	apiRouter.HandleFunc("/vets", services.GetVetsHandler).Methods("GET")
 	apiRouter.HandleFunc("/vets/{id}", services.GetVetHandler).Methods("GET")
+	apiRouter.HandleFunc("/vets/Professionals/{id}", services.GetProfessinalsVet).Methods("GET")
 	apiRouter.HandleFunc("/vets", services.CreateVetHandler).Methods("POST")
 }
--- services/vets.service.go
+++ services/vets.service.go
@@ -27,6 +27,24 @@ func GetVetHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(&vet)
 }
 
+func GetProfessinalsVet(w http.ResponseWriter, r *http.Request) {
+	var partner models.Partner
+	params := mux.Vars(r)
+	db.DB.Preload("Vets").First(&partner, params["id"])
+	if partner.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Partner Not Found"))
+		return
+	}
+	db.DB.Preload("Professionals").First(&partner.Vets, partner.Vets[0].ID)
+	if partner.Vets == nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Vet Not Found"))
+		return
+	}
+	json.NewEncoder(w).Encode(&partner.Vets[0].Professionals)
+}
+
 func CreateVetHandler(w http.ResponseWriter, r *http.Request) {
 	var vet models.Vet
 	json.NewDecoder(r.Body).Decode(&vet)
