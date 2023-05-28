943b17d8aa1e54cf9cb488a88f890bc314f72cff
Julian Ardila <julian07099@gmail.com>
2023-05-28 18:58:03 -0500

    Added BasicServices and OptionalServices

--- models/Vet.go
+++ models/Vet.go
@@ -7,16 +7,18 @@ import (
 type Vet struct {
 	gorm.Model
 	NameVet          string         `gorm:"not null" json:"name_vet"`
-	Location         string         `gorm:"not null" json:"location_vet"`
-	City             string         `gorm:"not null" json:"city_vet"`
-	Country          string         `gorm:"not null" json:"country_vet"`
-	Presentation     string         `gorm:"not null" json:"presentation_vet"`
+	Location         string         `json:"location_vet"`
+	City             string         `json:"city_vet"`
+	Country          string         `json:"country_vet"`
+	Presentation     string         `json:"presentation_vet"`
 	Description      string         `json:"description_vet"`
 	ShortDescription string         `json:"short_description_vet"`
 	Image            string         `json:"image_vet"`
-	Telephone        uint           `gorm:"not null" json:"telephone_vet"`
+	Telephone        uint           `json:"telephone_vet"`
 	Blog             []Post         `json:"blog_vet"`
 	Professionals    []Professional `json:"profesionals_vet"`
 	Appointment      []Appointment  `json:"appointment_vet"`
 	PartnerID        uint           `json:"partner_id_vet"`
+	BasicServices    string         `json:"basic_services_vet"`
+	OptionalServices string         `json:"optional_services_vet"`
 }
