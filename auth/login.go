11b54c7c15768698f9ed588cfd93be802eb28b04
Julian Ardila <julian07099@gmail.com>
2023-05-29 00:30:38 -0500

    Removed validations for user attendant

--- auth/login.go
+++ auth/login.go
@@ -34,35 +34,30 @@ func AuthenticatePartnerHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var partner models.Partner
-	var attendant models.Attendant
+	// var attendant models.Attendant
 	db.DB.Preload("Vets").Where("username = ?", creds.Username).First(&partner)
-	db.DB.Where("username = ?", creds.Username).First(&attendant)
+	// db.DB.Where("username = ?", creds.Username).First(&attendant)
 	if partner.ID == 0 || !checkPasswordHash(creds.Password, partner.Password) {
-		if attendant.ID == 0 || !checkPasswordHash(creds.Password, partner.Password) {
-			w.WriteHeader(http.StatusUnauthorized)
-			w.Write([]byte("Invalid Credentials"))
-			return
-		}
+		// if attendant.ID == 0 || !checkPasswordHash(creds.Password, partner.Password) {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("Invalid Credentials"))
+		return
+		// }
 	}
 
 	var nitValue int
 	if partner.Nit != 0 {
 		nitValue = 1
-	} 
-	if attendant.ID != 0 {
+	} else {
 		nitValue = 0
 	}
-	
 
 	token := jwt.New(jwt.SigningMethodHS256)
 
 	claims := token.Claims.(jwt.MapClaims)
-	if nitValue == 1 {
-		// claims["partner_id"] = partner.ID
-		claims["vet_id"] = partner.Vets[0].ID
-	} else {
-		claims["user_id"] = attendant.ID
-	}
+	
+	claims["partner_id"] = partner.ID
+		// claims["vet_id"] = partner.Vets[0].ID
 	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
 
 	tokenString, err := token.SignedString([]byte(mySecretKey))
