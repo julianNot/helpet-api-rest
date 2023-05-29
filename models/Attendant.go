35c85d459d33e26a75180e5dae07e66c4867ebf9
Julian Ardila <julian07099@gmail.com>
2023-05-29 18:52:40 -0500

    Fixed Attendant model

--- models/Attendant.go
+++ models/Attendant.go
@@ -6,7 +6,7 @@ type Attendant struct {
 	gorm.Model
 	Name        string        `gorm:"not null" json:"name_attendant"`
 	Telephone   uint          `gorm:"not null" json:"telephone_attendant"`
-	SaveBlogs   []Post        `json:"saved_blogs_attendant"`
+	// SaveBlogs   []Post        `json:"saved_blogs_attendant"`
 	Pet         []Pet         `json:"pets_attendant"`
 	Appointment []Appointment `json:"appointments_attendant"`
 	NamePet     string        `gorm:"default:'Sin nombre'" json:"name_pet_attendant"`
